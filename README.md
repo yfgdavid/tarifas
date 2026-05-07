b200387968a7e44a098c166e39a70d23dded1488
David Victor <davidvictorcontato7@gmail.com>
2026-05-06 23:52:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tarifas
+API REST para gerenciamento e cálculo de tarifas de água por faixas de consumo progressivas. Desafio técnico com Spring Boot + PostgreSQL.
---
+++ src/main/java/com/example/tarifas/TarifasApplication.java
@@ -0,0 +1,13 @@
+package com.example.tarifas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarifasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TarifasApplication.class, args);
+    }
+
+}
